7b486bfdd70756395bdc94f2dc7fd7da6fde9ce2
Anurag <anuktsksanurag@gmail.com>
2025-04-18 11:00:52 +0530

    fixes

--- app/src/main/java/com/example/pupilmeshprojects/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/pupilmeshprojects/presentation/sign_in/SignInScreen.kt
@@ -1,13 +1,17 @@
 package com.example.pupilmeshprojects.presentation.sign_in
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
@@ -16,13 +20,34 @@ import androidx.compose.ui.unit.dp
 fun SignInScreen(viewModel: SignInViewModel, onSignedIn: () -> Unit) {
     if (viewModel.isSignedIn) onSignedIn()
 
-    Column(Modifier.padding(16.dp)) {
-        OutlinedTextField(value = viewModel.email, onValueChange = { viewModel.email = it }, label = { Text("Email") })
-        OutlinedTextField(value = viewModel.password, onValueChange = { viewModel.password = it }, label = { Text("Password") }, visualTransformation = PasswordVisualTransformation())
-        Button(onClick = viewModel::onSignInClick, Modifier.padding(top = 16.dp)) {
-            Text("Sign In")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = viewModel.email,
+                onValueChange = { viewModel.email = it },
+                label = { Text("Email") }
+            )
+            OutlinedTextField(
+                value = viewModel.password,
+                onValueChange = { viewModel.password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(
+                onClick = viewModel::onSignInClick,
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text("Sign In")
+            }
         }
     }
 }
+
 
 
